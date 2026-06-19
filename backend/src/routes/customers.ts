33716a09d69efc215c3cd0a3a3b4cceddc2d71bf
veraulitinaYa <veraulitina@yandex.ru>
2026-06-19 11:14:16 +0300

    Revert "fix customer router"
    This reverts commit 9da4e8614436244063cd69bbdbfcda8e2598ddb4.

--- backend/src/routes/customers.ts
+++ backend/src/routes/customers.ts
@@ -5,37 +5,13 @@ import {
     getCustomers,
     updateCustomer,
 } from '../controllers/customers'
-import auth, { roleGuardMiddleware } from '../middlewares/auth'
-import { Role } from '../models/user'
+import auth from '../middlewares/auth'
 
 const customerRouter = Router()
 
-customerRouter.get(
-    '/',
-    auth,
-    roleGuardMiddleware(Role.Admin),
-    getCustomers
-)
-
-customerRouter.get(
-    '/:id',
-    auth,
-    roleGuardMiddleware(Role.Admin),
-    getCustomerById
-)
-
-customerRouter.patch(
-    '/:id',
-    auth,
-    roleGuardMiddleware(Role.Admin),
-    updateCustomer
-)
-
-customerRouter.delete(
-    '/:id',
-    auth,
-    roleGuardMiddleware(Role.Admin),
-    deleteCustomer
-)
+customerRouter.get('/', auth, getCustomers)
+customerRouter.get('/:id', auth, getCustomerById)
+customerRouter.patch('/:id', auth, updateCustomer)
+customerRouter.delete('/:id', auth, deleteCustomer)
 
 export default customerRouter
