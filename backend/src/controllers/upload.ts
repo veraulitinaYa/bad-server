d212decb6f7b3c197e48873290db5eb6c0142e67
veraulitinaYa <veraulitina@yandex.ru>
2026-06-19 11:57:57 +0300

    fix image files

--- backend/src/controllers/upload.ts
+++ backend/src/controllers/upload.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express'
 import { constants } from 'http2'
 import BadRequestError from '../errors/bad-request-error'
+import sharp from 'sharp'
 
 export const uploadFile = async (
     req: Request,
@@ -10,24 +11,35 @@ export const uploadFile = async (
     if (!req.file) {
         return next(new BadRequestError('Файл не загружен'))
     }
-    if (req.file.size < 2048) {
-    return next(
-        new BadRequestError(
-            'Размер файла должен быть больше 2kb'
-        )
-    )
-}
+
     try {
+        if (req.file.size < 2048) {
+            return next(
+                new BadRequestError('Файл слишком маленький')
+            )
+        }
+
+        const metadata = await sharp(req.file.path).metadata()
+
+        if (!metadata.width || !metadata.height) {
+            return next(
+                new BadRequestError(
+                    'Некорректное изображение'
+                )
+            )
+        }
+
         const fileName = process.env.UPLOAD_PATH
             ? `/${process.env.UPLOAD_PATH}/${req.file.filename}`
-            : `/${req.file?.filename}`
+            : `/${req.file.filename}`
+
         return res.status(constants.HTTP_STATUS_CREATED).send({
             fileName,
-            originalName: req.file?.originalname,
+            originalName: req.file.originalname,
         })
     } catch (error) {
-        return next(error)
+        return next(
+            new BadRequestError('Некорректный файл изображения')
+        )
     }
 }
-
-export default {}
